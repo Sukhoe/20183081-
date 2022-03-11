1b3d18edc58ff139adba2ff8c19b93f50448a93b
20183081 이석호 <101093909+Sukhoe@users.noreply.github.com>
2022-03-11 09:28:46 +0900

    Add files via upload

---
+++ math homework 1-4.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+A = np.array([[3,0],
+              [-1,2],
+              [1,1]])
+
+B = np.array([[-3,-1],
+              [2,1],
+              [4,3]])
+
+C = np.array([[1,2,3],
+              [2,0,1]])
+
+D = np.array([[1],
+              [2],
+              [3]])
+
+AC = np.dot(A,C)
+
+
+
+print("(b) AC = \n", AC)
+
